987ac8d6839217dffa4679aa47d0f38127ac9c6d
eissko <eissko@gmail.com>
2020-06-26 13:49:32 +0200

    adding capability of dynamically passing next_hop_ip instead of only reading it from tfvars file

--- variables.tf
+++ variables.tf
@@ -6,3 +6,8 @@ variable "route_table" {
    description = "(Required) route table object to be created"
  
 }
+
+variable next_hop_in_ip_address {
+   description = "(Optional) dynamically passing gateway ip which is output of another terraform resource, e.g. azure firewall private ip"
+   default = null
+}

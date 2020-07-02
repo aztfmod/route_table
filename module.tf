720da6dab4526c3db267032766b39e48ce9d227f
eissko <eissko@gmail.com>
2020-07-02 15:19:59 +0200

    support address prefix as array

--- module.tf
+++ module.tf
@@ -1,19 +1,35 @@
 #Reference https://www.terraform.io/docs/providers/azurerm/r/route_table.html
 
 resource "azurerm_route_table" "route_table" {
-  name                          = var.route_table.name
+  for_each = var.route_table
+
+  name                          = each.value["name"]
   location                      = var.location
   resource_group_name           = var.resource_group_name
   tags                          = local.tags
   disable_bgp_route_propagation = lookup(var.route_table, "disable_bgp_route_propagation", null)
 
   dynamic "route" {
-    for_each = var.route_table.route_entries
+    for_each = flatten(
+      [
+          for route_key,route_value in each.value["route_entries"]:
+          [
+            for address_key, address_value in flatten(list(route_value.address_prefix)):
+            {
+              name                   = replace(join("-",list(route_value.name,address_value)),"/","_")
+              address_prefix         = address_value
+              next_hop_type          = route_value.next_hop_type
+              next_hop_in_ip_address = route_value.next_hop_type == "VirtualAppliance" ? (var.next_hop_in_dynamic_private_ip != null && var.next_hop_in_dynamic_private_ip != "null" && var.next_hop_in_dynamic_private_ip != "" ? var.next_hop_in_dynamic_private_ip : route.value.next_hop_in_ip_address) : null
+            }
+          ]
+      ]
+    )
+    
     content {
       name                   = route.value.name
-      address_prefix         = route.value.prefix
+      address_prefix         = route.value.address_prefix
       next_hop_type          = route.value.next_hop_type
-      next_hop_in_ip_address = route.value.next_hop_type == "VirtualAppliance" ? (var.next_hop_in_dynamic_private_ip != null && var.next_hop_in_dynamic_private_ip != "null" && var.next_hop_in_dynamic_private_ip != "" ? var.next_hop_in_dynamic_private_ip : route.value.next_hop_in_ip_address) : null
+      next_hop_in_ip_address = route.value.next_hop_in_ip_address
     }
   }
 }

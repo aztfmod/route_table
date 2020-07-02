125cdfcf29971288ffbc83467d7845eac0897650
eissko <eissko@gmail.com>
2020-07-03 00:11:25 +0200

    update readme

--- main.tf
+++ main.tf
@@ -11,9 +11,9 @@ locals {
 
 terraform {
   required_providers {
-    # azurecaf = {
-    #   source = "aztfmod/azurecaf"
-    # }
+    azurecaf = {
+      source = "aztfmod/azurecaf"
+    }
     azurerm = {
       source = "hashicorp/azurerm"
     }

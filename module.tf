0c2c43a0588763470b05fa8f4632a84e42e72fdc
eissko <eissko@gmail.com>
2020-07-02 23:39:15 +0200

    fix variable name

--- module.tf
+++ module.tf
@@ -10,14 +10,14 @@ resource "azurerm_route_table" "route_table" {
   dynamic "route" {
     for_each = flatten(
       [
-          for route_key,route_value in var.route_table.route_entries:
+          for route_key,route_value in var.route_table.route_entries :
           [
             for address_key, address_value in flatten(list(route_value.address_prefix)): # flatten(list()) handles support for both array & string
             {
               name                   = replace(join("-",list(route_value.name,address_value)),"/","_") # build name from route table name + address prefix, escape slash
               address_prefix         = address_value
               next_hop_type          = route_value.next_hop_type
-              next_hop_in_ip_address = route_value.next_hop_type == "VirtualAppliance" ? (var.next_hop_in_dynamic_private_ip != null && var.next_hop_in_dynamic_private_ip != "null" && var.next_hop_in_dynamic_private_ip != "" ? var.next_hop_in_dynamic_private_ip : route.value.next_hop_in_ip_address) : null
+              next_hop_in_ip_address = route_value.next_hop_type == "VirtualAppliance" ? (var.next_hop_in_dynamic_private_ip != null && var.next_hop_in_dynamic_private_ip != "null" && var.next_hop_in_dynamic_private_ip != "" ? var.next_hop_in_dynamic_private_ip : route_value.next_hop_in_ip_address) : null
             }
           ]
       ]
